fc9217aa481b67cb9f274b6349bfb0c7277e444a
Ayaz Armutlu <69972517+ayazarm@users.noreply.github.com>
2022-03-19 12:29:09 +0300

    1.0

---
+++ İnstagram Follower List Bot/instabot.py
@@ -0,0 +1,24 @@
+
+import instaloader
+
+guc = instaloader.Instaloader()
+
+
+username = "" # your username gonna be came here / Kullanici isminizi buraya giriceksiniz
+password = "" # your password gonna be came here / Sifrenizi isminizi buraya giriceksiniz
+guc.login(username, password) 
+
+
+profile = instaloader.Profile.from_username(guc.context, username)
+
+
+follow_list = []
+count = 0
+for followee in profile.get_followers():
+    follow_list.append(followee.username)
+    file = open("takipciler.txt", "a+")
+    file.write(follow_list[count])
+    file.write("\n")
+    file.close()
+    print(follow_list[count])
+    count = count + 1
