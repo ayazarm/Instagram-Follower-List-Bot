2075711d8cee7c50c3ed87efddc2521c970051f9
Ayaz <ayazayazilim@gmail.com>
2022-05-03 22:07:43 +0300

    Update 2.0
    The list name is changed to today's date (auto changeable)

--- İnstagram Follower List Bot/instabot.py
+++ İnstagram Follower List Bot/instabot.py
@@ -18,7 +18,7 @@ follow_list = []
 count = 0
 for follomen in profile.get_followers():
     follow_list.append(follomen.username)
-    file = open("followers.txt", "a+")
+    file = open("{}.txt".format(today), "a+")
     file.write(follow_list[count])
     file.write("\n")
     file.close()
