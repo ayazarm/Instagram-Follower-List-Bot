8283b4aea854e1605eae035526a967befbd06c2c
Ayaz <ayazayazilim@gmail.com>
2022-05-03 21:59:20 +0300

    wont look that

--- İnstagram Follower List Bot/instabot.py
+++ İnstagram Follower List Bot/instabot.py
@@ -1,6 +1,9 @@
 import instaloader
+from datetime import date
+
 
 guc = instaloader.Instaloader()
+today = date.today()
 
 
 username = "" # your username gonna be came here / Kullanici isminizi buraya giriceksiniz
@@ -15,7 +18,7 @@ follow_list = []
 count = 0
 for follomen in profile.get_followers():
     follow_list.append(follomen.username)
-    file = open("takipciler.txt", "a+")
+    file = open("followers.txt", "a+")
     file.write(follow_list[count])
     file.write("\n")
     file.close()
